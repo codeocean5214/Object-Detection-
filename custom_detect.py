5ef06afed274c5e352b56c50c27903678b983ba5
ADITYA MISHRA <145369892+codeocean5214@users.noreply.github.com>
2025-09-21 23:45:05 +0530

    classifiers imported

--- custom_detect.py
+++ custom_detect.py
@@ -1,4 +1,10 @@
 import cv2 
 import numpy 
 import matplotlib.pyplot as plt 
+import os  # to avoid hardcoding the file
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") 
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+#import the custom classifires 
+#NOW WIRTE THE FUNCTION YOU WANT TO FOR THE CUSTOM DETECTION AND THEM START 
+
 
